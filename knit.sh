bb2ac6e9643c46114cba3ea2f27dd5912e5b9344
cjlangan <connorjameslangan@gmail.com>
2024-09-20 11:23:42 -0500

    initial commit

---
+++ knit.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Function to show help
+function show_help
+{
+    echo "Usage: $0 [-h] [-p] <filename>"
+    echo "  -h      knit to html"
+    echo "  -p      knit to pdf"
+}
+
+# Varibale to hold document output type
+type="html_document"
+
+# Determine flag for output type
+while getopts "hp" opt; do
+    case ${opt} in
+        h )
+            type="html_document"
+            ;;
+        p )
+            type="pdf_document"
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+# Shift parameters to point to the filename
+shift $((OPTIND -1))
+
+# Read filename
+file="$1"
+
+# Ensure we received input
+if [ -z "$1" ]; then
+    echo "Please provide a file name."
+    show_help
+    exit 1
+fi
+
+# Command to knit file to specifieid output type
+Rscript -e "library(rmarkdown); rmarkdown::render('${file}', '${type}')"
+
